e94afe62ff8adbfc9742be43479889eee2c772fe
Onimisi Adelza Adeolu <onimisiadeolu@gmail.com>
2025-11-16 15:51:46 +0100

    Fixed textures.tst

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,4 +1,6 @@
-import * as React from "react"import { cn } from "@/lib/utils"const Textarea = React.forwardRef<
+import * as React from "react"
+  import { cn } from "@/lib/utils"
+    const Textarea = React.forwardRef<
   HTMLTextAreaElement,
   React.ComponentProps<"textarea">>(({ className, ...props }, ref) => {
   return (
